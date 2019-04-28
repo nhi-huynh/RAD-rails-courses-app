65c89fd7bc75a14146bb7c61f9537375c54d9ee8
nhi-huynh <uyennhi.huynhluu@gmail.com>
2019-04-28 06:09:58 +0000

    Scaffold course without sample data

---
+++ db/migrate/20190428060841_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :courses do |t|
+      t.string :name
+      t.string :prerequisite
+      t.integer :likes
+      t.integer :dislikes
+
+      t.timestamps
+    end
+  end
+end
