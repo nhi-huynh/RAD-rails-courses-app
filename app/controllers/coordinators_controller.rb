e3915f402173e27aaee8f7717e1a992c78d73fec
EC2 Default User <ec2-user@ip-172-31-43-212.us-east-2.compute.internal>
2019-05-28 04:12:46 +0000

    coordinators_img

--- app/controllers/coordinators_controller.rb
+++ app/controllers/coordinators_controller.rb
@@ -69,6 +69,6 @@ class CoordinatorsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coordinator_params
-      params.require(:coordinator).permit(:name, :email, :password_digest)
+      params.require(:coordinator).permit(:name, :email, :password_digest,:photo_url)
     end
 end
--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,4 +1,5 @@
 class Coordinator < ApplicationRecord
     has_secure_password
     has_many :courses
+    # mount_uploader :image, ImageUploader
 end
